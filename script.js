09de98269175de4b5d364b1591c3c67fea140e81
AbdulMoiz <abdulmoizcs20@gmail.com>
2026-09-02 11:18:24 +0500

    Few changes

---
+++ script.js
@@ -0,0 +1,17 @@
+const title =  document.getElementById("title");
+const amount =  document.getElementById("amount");
+const category =  document.getElementById("category");
+const date =  document.getElementById("date");
+const addExpenseBtn =  document.getElementById("add-expense-btn");
+const editBtn =  document.getElementById("edit-btn");
+const delBtn =  document.getElementById("del-btn");
+
+const expenseitems = [];
+
+
+const list_item='';
+
+addExpenseBtn.addEventListener("click", () =>
+{
+    // expenseitems.push({});
+});
